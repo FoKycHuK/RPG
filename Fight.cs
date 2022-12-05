4c08eabb42b651eeed39369a6836e256317551a4
azagnoiko <alexander.zagnoiko@veeam.com>
2022-12-05 16:44:15 +0100

    HP count position in fights adjusted to 2k resolution

--- Fight.cs
+++ Fight.cs
@@ -26,7 +26,7 @@ namespace MyRPG
             this.player = player;
             this.monster = monster;
             monsterIcon = (Bitmap)Bitmap.FromFile("Images\\" + monster.GetCreatureType().ToString() + ".png");
-            ClientSize = new Size(ElementSize * 5, ElementSize * 2);
+            ClientSize = new Size(ElementSize * 8, ElementSize * 2);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             Text = "FIGHT";
             DoubleBuffered = true;
@@ -41,10 +41,10 @@ namespace MyRPG
             e.Graphics.TranslateTransform(0, 0);
             e.Graphics.FillRectangle(Brushes.Black, 0, 0, ClientSize.Width, ClientSize.Height);
             e.Graphics.DrawImage(playerIcon, 0, 0);
-            e.Graphics.DrawImage(monsterIcon, ClientSize.Width - ElementSize, 0);
+            e.Graphics.DrawImage(monsterIcon, ClientSize.Width - ElementSize - 10, 0);
             e.Graphics.ResetTransform();
             e.Graphics.DrawString("HP: " + player.hp.ToString(), new Font("Arial", 16), Brushes.Red, 0, ElementSize);
-            e.Graphics.DrawString("HP: " + monster.hp.ToString(), new Font("Arial", 16), Brushes.Red, ClientSize.Width - ElementSize * 2 - 10 , ElementSize);
+            e.Graphics.DrawString("HP: " + monster.hp.ToString(), new Font("Arial", 16), Brushes.Red, ClientSize.Width - ElementSize * 4 , ElementSize);
             e.Graphics.DrawString("VS", new Font("Arial", 16), Brushes.White, ElementSize * 2, 0);
 
         }
